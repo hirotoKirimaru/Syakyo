3228dac7ba8b2f9cbf7a895dd61a6070ed9683fb
nainaistar <nainaistar@gmail.com>
2020-08-22 22:28:15 +0900

    [Add] 第13章 92p

---
+++ syakyo/src/main/java/com/example/demo/tdd/Sum.java
@@ -0,0 +1,17 @@
+package com.example.demo.tdd;
+
+public class Sum implements Expression {
+  /**
+   * 被加算数(足し算で足される方の数)
+   */
+  Money augend;
+  /**
+   * 加数
+   */
+  Money addend;
+
+  public Sum(Money augend, Money addend) {
+    this.augend = augend;
+    this.addend = addend;
+  }
+}
