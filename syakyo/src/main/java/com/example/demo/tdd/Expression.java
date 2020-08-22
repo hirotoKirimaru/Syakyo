1dc86b6ec5fc1c57576e7ec3e7224b38e5211eed
nainaistar <nainaistar@gmail.com>
2020-08-22 22:39:16 +0900

    [Add] 第13章 99p

--- syakyo/src/main/java/com/example/demo/tdd/Expression.java
+++ syakyo/src/main/java/com/example/demo/tdd/Expression.java
@@ -1,4 +1,5 @@
 package com.example.demo.tdd;
 
 public interface Expression {
+  Money reduce(String to);
 }
--- syakyo/src/test/java/com/example/demo/tdd/MoneyTest.java
+++ syakyo/src/test/java/com/example/demo/tdd/MoneyTest.java
@@ -22,13 +22,13 @@ public class MoneyTest {
   }
 
   @Test
-  public void testCurrency(){
+  public void testCurrency() {
     assertEquals("USD", Money.dollar(1).currency());
     assertEquals("CHF", Money.franc(1).currency());
   }
 
   @Test
-  public void testSimpleAddition(){
+  public void testSimpleAddition() {
     Money five = Money.dollar(5);
     Expression sum = five.plus(five);
 
@@ -38,11 +38,26 @@ public class MoneyTest {
   }
 
   @Test
-  public void testPlusReturnsSum(){
+  public void testPlusReturnsSum() {
     Money five = Money.dollar(5);
     Expression result = five.plus(five);
     Sum sum = (Sum) result;
     assertEquals(five, sum.augend);
     assertEquals(five, sum.addend);
+  }
+
+  @Test
+  public void testReduceSum() {
+    Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+    Bank bank = new Bank();
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(7), result);
+  }
+
+  @Test
+  public void testReduceMoney() {
+    Bank bank = new Bank();
+    Money result = bank.reduce(Money.dollar(1), "USD");
+    assertEquals(Money.dollar(1), result);
   }
 }
