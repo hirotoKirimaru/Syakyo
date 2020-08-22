de8d3b1c12220ea69c15fdad3485964ef86696b1
nainaistar <nainaistar@gmail.com>
2020-08-22 23:17:56 +0900

    [Add] 第15章 125p

--- syakyo/src/test/java/com/example/demo/tdd/MoneyTest.java
+++ syakyo/src/test/java/com/example/demo/tdd/MoneyTest.java
@@ -83,4 +83,16 @@ public class MoneyTest {
     Money result = bank.reduce(fiveBucks.plus(tenFranc), "USD");
     assertEquals(Money.dollar(10), result);
   }
+
+  @Test
+  public void testSumPlusMoney(){
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFranc = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    // SUM使わずに書けるが、意図を示すためにわざと使用している。
+    Expression sum = new Sum(fiveBucks, tenFranc).plus(fiveBucks);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(15), result);
+  }
 }
