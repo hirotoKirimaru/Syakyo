a9084e41428d49bc6419a589098ed9b7e15f2955
nainaistar <nainaistar@gmail.com>
2020-08-22 23:00:38 +0900

    [Add] 第14章 109p

--- syakyo/src/main/java/com/example/demo/tdd/Bank.java
+++ syakyo/src/main/java/com/example/demo/tdd/Bank.java
@@ -1,11 +1,21 @@
 package com.example.demo.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+  private Map<Pair, Integer> rates = new HashMap<>();
+
   public Money reduce(Expression source, String to) {
     return source.reduce(this, to);
   }
 
-  public void addRate(String chf, String usd, int i) {
-//    int rate = (currency.equals("CHF") &&)
+  public void addRate(String from, String to, int rate) {
+    rates.put(new Pair(from, to), rate);
+  }
+
+  int rate(String from, String to) {
+    if (from.equals(to)) return 1;
+    return rates.get(new Pair(from,to));
   }
 }
