fcd05fac324585f90f5ed923a14b96207e5f38d8
nainaistar <nainaistar@gmail.com>
2020-08-23 10:40:09 +0900

    [Add] 第22章 170P

--- python/tdd/xunit.py
+++ python/tdd/xunit.py
@@ -30,10 +30,13 @@ class WasRun(TestCase):
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
     def testStarted(self):
         self.runCount = self.runCount + 1
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class TestCaseTest(TestCase):
     def testTemplateMethof(self):
@@ -48,8 +51,13 @@ class TestCaseTest(TestCase):
         test = WasRun("testBrokenMethod")
         result = test.run()
         assert ("1 run, 1 failed" == result.summary())
-
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert ("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
 # TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
