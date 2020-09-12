5e0635e02618bc3a1f89fd11ef4289eed7630416
nainaistar <nainaistar@gmail.com>
2020-09-12 13:45:25 +0900

    [Add] EffectiveJava 第二章項目13 61P

--- syakyo/src/main/java/com/example/demo/effectiveJava/second_13/PhoneNumber.java
+++ syakyo/src/main/java/com/example/demo/effectiveJava/second_13/PhoneNumber.java
@@ -3,6 +3,8 @@ package com.example.demo.effectiveJava.second_13;
 public class PhoneNumber {
 
   // 可変な状態への参照を持たないクラスのcloneメソッド
+  // super.cloneはObjectなので、コンパイルするとObject型を返却するcloneメソッドが生成される。
+  // overrideをすると、こっち？
   @Override
   public PhoneNumber clone() {
     try {
