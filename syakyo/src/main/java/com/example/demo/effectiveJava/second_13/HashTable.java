a2e0c3703c669c1a7f954884072480fa9d423f32
nainaistar <nainaistar@gmail.com>
2020-09-12 14:09:38 +0900

    [Add] EffectiveJava 第二章項目13 65P

--- syakyo/src/main/java/com/example/demo/effectiveJava/second_13/HashTable.java
+++ syakyo/src/main/java/com/example/demo/effectiveJava/second_13/HashTable.java
@@ -3,7 +3,7 @@ package com.example.demo.effectiveJava.second_13;
 public class HashTable implements Cloneable {
   private Entry[] buckets;
 
-  private static class Entry{
+  private static class Entry {
     final Object key;
     Object value;
     Entry next;
@@ -13,14 +13,33 @@ public class HashTable implements Cloneable {
       this.value = value;
       this.next = next;
     }
+
+    Entry deepCopy() {
+      return new Entry(key, value, next == null ? null : next.deepCopy());
+    }
   }
 
-  // 不完全 - 内部状態が共有されている
+  //  // 不完全 - 内部状態が共有されている
+//  @Override
+//  public HashTable clone() {
+//    try {
+//      HashTable result = (HashTable) super.clone();
+//      result.buckets = buckets.clone();
+//      return result;
+//    } catch (CloneNotSupportedException e) {
+//      throw new AssertionError(); // 起こりえない
+//    }
+//  }
+// 不完全 - 内部状態が共有されている
   @Override
   public HashTable clone() {
     try {
       HashTable result = (HashTable) super.clone();
-      result.buckets = buckets.clone();
+      result.buckets = new Entry[buckets.length];
+      // リンクリストを複製した時は、スタックフレームを消費するから、リストが長いとスタックオーバーフローを起こしうる。
+      for (int i = 0; i < buckets.length; i++) {
+        if (buckets[i] != null) result.buckets[i] = buckets[i].deepCopy();
+      }
       return result;
     } catch (CloneNotSupportedException e) {
       throw new AssertionError(); // 起こりえない
