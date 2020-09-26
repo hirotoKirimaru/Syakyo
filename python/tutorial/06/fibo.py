ebe66ad6f80c37b29b086fb36214955968221eb5
kirimaru <nainaistar@gmail.com>
2020-09-26 01:13:18 +0000

    [Add] 6.1.1

--- python/tutorial/06/fibo.py
+++ python/tutorial/06/fibo.py
@@ -14,3 +14,8 @@ def fib2(n):   # return Fibonacci series up to n
         result.append(a)
         a, b = b, a+b
     return result
+
+# スクリプト化できる。モジュールをimportしても実行されない。
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
