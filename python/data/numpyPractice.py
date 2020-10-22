9d18fc8b1d7e94b7606f27e9f78bb4f1fc7e3478
kirimaru <nainaistar@gmail.com>
2020-10-22 02:45:59 +0000

    p93

--- python/data/numpyPractice.py
+++ python/data/numpyPractice.py
@@ -5,3 +5,19 @@ a = np.array([1,2,3])
 print(a)
 print(type(a))
 print(a.shape)
+
+b = np.array([[1,2,3],[4,5,6]])
+print(b)
+print(b.shape)
+
+c1 = np.arange(6)
+print(c1)
+
+c2 = c1.reshape((2,3))
+print(c2)
+
+c3 = c2.ravel()
+print(c3)
+
+c4 = c2.flatten()
+print(c4)
